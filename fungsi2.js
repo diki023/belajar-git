fcff45fc90639d5c2344d7e0f30fea70d3d4b546
diki <andreandicky022@gmail.com>
2024-09-27 16:16:08 +0700

    merevisi commit perkalian

--- fungsi2.js
+++ fungsi2.js
@@ -4,10 +4,14 @@ const add = (a,b) => a + b;
 console.log(add(1,4))
 
 const subtrak = (a,b) => a - b;
+<<<<<<< HEAD
 console.log(subtrak(7,3))
 
-const multiplay = (a,b) => a * b;
-console.log(multiplay(2,3))
+const multiplay1 = (a,b) => a * b;
+console.log(multiplay1(2,3))
 
 const divide = (c,d) => c / d;
 console.log(divide(4,2))
+=======
+console.log(subtrak(7,3))
+>>>>>>> parent of 07ad2a8 (menambahkan fitur perkalian)
