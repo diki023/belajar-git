49f1510e90f3156a55da5249256d40da58f1faeb
diki <andreandicky022@gmail.com>
2024-09-27 16:35:49 +0700

    tambah fungsi cek add

--- fungsi2.js
+++ fungsi2.js
@@ -4,7 +4,6 @@ const add = (a,b) => a + b;
 console.log(add(1,4))
 
 const subtrak = (a,b) => a - b;
-<<<<<<< HEAD
 console.log(subtrak(7,3))
 
 const multiplay1 = (a,b) => a * b;
@@ -12,6 +11,6 @@ console.log(multiplay1(2,3))
 
 const divide = (c,d) => c / d;
 console.log(divide(4,2))
-=======
 console.log(subtrak(7,3))
->>>>>>> parent of 07ad2a8 (menambahkan fitur perkalian)
+
+console.log(add(9,9))
